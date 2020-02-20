a7c9a92e6a705a2a25a9745838c16ec30858f56b
ieieiei59 <yota0509a@gmail.com>
2020-02-20 22:15:08 +0900

    ghpagesに合わせて修正

--- models/mock/TakoyakiItem.js
+++ models/mock/TakoyakiItem.js
@@ -3,7 +3,7 @@ export default [
     id: 1,
     name: 'ノーマルタコヤキ',
     rarity: 3,
-    imgURL: '/takoyaki_todo/image/takoyaki/takoyaki_character.png',
+    imgURL: '/TakoyakiTodo/image/takoyaki/takoyaki_character.png',
     addedAt: new Date('2020-02-03T06:00:00'),
     editedAt: new Date('2020-02-03T06:00:00')
   },
@@ -11,7 +11,7 @@ export default [
     id: 2,
     name: 'コゲ太',
     rarity: 1,
-    imgURL: '/takoyaki_todo/image/takoyaki/failed.png',
+    imgURL: '/TakoyakiTodo/image/takoyaki/failed.png',
     addedAt: new Date('2020-02-09T01:07:00'),
     editedAt: new Date('2020-02-09T01:07:00')
   }
--- nuxt.config.js
+++ nuxt.config.js
@@ -6,28 +6,36 @@ export default {
    ** Headers of the page
    */
   router: {
-    base: '/takoyaki_todo/'
+    base: '/TakoyakiTodo/'
   },
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
       {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/takoyaki_todo/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/TakoyakiTodo/favicon.ico'
+    }]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: {
+    color: '#fff'
+  },
   /*
    ** Global CSS
    */
